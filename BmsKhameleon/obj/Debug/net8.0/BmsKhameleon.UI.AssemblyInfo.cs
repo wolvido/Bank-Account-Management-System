893fdef24c9f03328fd0d148611aae51e477b7b2
Winzyl Olvido <winzyl.olvido@mobilegroupinc.com>
2024-05-29 15:04:17 +0800

    fixed font and added skeleton for create bank account page

--- BmsKhameleon/obj/Debug/net8.0/BmsKhameleon.UI.AssemblyInfo.cs
+++ BmsKhameleon/obj/Debug/net8.0/BmsKhameleon.UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BmsKhameleon.UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a6bca89591a7b68e105580c46ee2f2e73f8a91c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b9b2d4b3b13b5539c9489bcda3f0252235312b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("BmsKhameleon.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BmsKhameleon.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
