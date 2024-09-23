a3a7c24fa804606d5d792743a1885ea34b68f5aa
Winzyl Olvido <winzyl.olvido@mobilegroupinc.com>
2024-09-23 17:37:21 +0800

    radio validation indicator implemented

--- BmsKhameleon.Core/obj/Release/net8.0/BmsKhameleon.Core.AssemblyInfo.cs
+++ BmsKhameleon.Core/obj/Release/net8.0/BmsKhameleon.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BmsKhameleon.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b4c3e4058f356706731999fd8f68d217306ced1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6be95b33452c9bb2ab83ce2c20cef88ca0f87d7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BmsKhameleon.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BmsKhameleon.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- BmsKhameleon.Infrastructure/obj/Debug/net8.0/BmsKhameleon.Infrastructure.AssemblyInfo.cs
+++ BmsKhameleon.Infrastructure/obj/Debug/net8.0/BmsKhameleon.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BmsKhameleon.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b4c3e4058f356706731999fd8f68d217306ced1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6be95b33452c9bb2ab83ce2c20cef88ca0f87d7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BmsKhameleon.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BmsKhameleon.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
